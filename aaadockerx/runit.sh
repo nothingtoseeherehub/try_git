080942b11106a270e240600991cf5c3396e1cb2b
Git Lap <gitlap@nethole.com>
2017-05-12 10:18:59 +0000

    mapped userid to 1001

--- aaadockerx/runit.sh
+++ aaadockerx/runit.sh
@@ -1,4 +1,6 @@
 docker run -ti --rm \
        -e DISPLAY=$DISPLAY \
+       --net=host \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -v /home/user/.Xauthority:/home/user/.Xauthority \
        infrastructurejames/aaax:v1 "$@"
