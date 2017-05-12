efd696c45b08d4bf7d606240cd2a19337f713628
Git Lap <gitlap@nethole.com>
2017-05-12 10:58:47 +0000

    permission issues

--- aaadockerx/runit.sh
+++ aaadockerx/runit.sh
@@ -4,3 +4,8 @@ docker run -ti --rm \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v /home/user/.Xauthority:/home/user/.Xauthority \
        infrastructurejames/aaax:v1 "$@"
+
+
+
+# considered -v /home/user/triplea:/home/user/triplea \
+# but permission issues unresolved
