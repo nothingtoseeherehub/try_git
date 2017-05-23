8f51eb34217f28970cb20446fcf465f7ea699386
Git Lap <gitlap@nethole.com>
2017-05-23 18:22:59 +0000

    recognize issues moving containers to different systems with different directories for sharing

--- aaadockerx/runit.sh
+++ aaadockerx/runit.sh
@@ -3,9 +3,15 @@ docker run -ti --rm \
        --net=host \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v /home/user/.Xauthority:/home/user/.Xauthority \
+       -v /home/user/triplea:/home/user/triplea \
        infrastructurejames/aaax:v1 "$@"
 
 
 
 # considered -v /home/user/triplea:/home/user/triplea \
 # but permission issues unresolved
+
+# will need to consider method to get userid provided on various systems
+# possibly with different directory names
+
+
